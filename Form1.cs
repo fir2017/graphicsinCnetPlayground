15e64ff6c9836f82aef92b7de08f759107876ce0
fir2017 <razvantrufin99x@gmail.com>
2021-03-23 03:22:59 +0200

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -6,8 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
-namespace graphicsplayground
+using graphicswithpermanentcontrol;
+namespace grafice
 {
     public partial class Form1 : Form
     {
@@ -16,62 +16,9 @@ namespace graphicsplayground
             InitializeComponent();
         }
 
-        private void peintedcontrol1_Paint(object sender, PaintEventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            peintedcontrol2.SetMode("NONE");
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            peintedcontrol2.SetMode("DOT");
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            peintedcontrol2.SetMode("LINE");
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            peintedcontrol2.SetMode("SQUARE");
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            peintedcontrol2.SetMode("ARC");
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            peintedcontrol2.SetMode("CIRCLE");
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            peintedcontrol2.SetMode("ELIPSE");
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            peintedcontrol2.textuldeafisat = this.textBox1.Text;
-            peintedcontrol2.SetMode("TEXT");
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            if (peintedcontrol2.EnterInDebugMode == false)
-            {
-                peintedcontrol2.SetDebugModeOnOff("ON");
-            }
-            else
-            {
-                peintedcontrol2.SetDebugModeOnOff("OFF");
-            }
+            this.userControl11.SetMode ("DOT");
         }
     }
 }
